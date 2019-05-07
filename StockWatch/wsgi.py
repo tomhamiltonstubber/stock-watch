1931d635bbade60e8b5b7a2566f8cbe8519c6f5c
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2019-05-07 21:43:49 +0100

    Deploying to heroku

--- StockWatch/wsgi.py
+++ StockWatch/wsgi.py
@@ -1,15 +1,6 @@
-"""
-WSGI config for StockWatch project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StockWatch.settings")
 
